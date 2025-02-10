a9fa4ab5b31e6f1573a272412700431f9280c793
Alžbeta Hajná <ahajna@typesoft.io>
2025-02-10 11:37:08 +0100

    refactor vibration to be in column

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,30 +9,36 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Custom Vibration Demo')),
-      body: Center(
-        // TODO(betka): extract to HandlingFutureBuilder
-        child: FutureBuilder<bool?>(
-            // TODO(betka): support also non-customizable vibrations (Vibration.hasVibration())
-            future: Vibration.hasCustomVibrationsSupport(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return Text('While finding Vibrator an error occurred.');
-              }
-
-              if (!snapshot.hasData) {
-                return CircularProgressIndicator();
-              }
-
-              final hasVibrator = snapshot.data ?? false;
-
-              if (!hasVibrator) {
-                return Text('Your device does not support customized vibrations.');
-              }
-
-              return _buildVibrationContent();
-            }),
+      body: Column(
+        children: [
+          Expanded(child: _buildVibration()),
+        ],
       ),
     );
+  }
+
+  FutureBuilder<bool?> _buildVibration() {
+    // TODO(betka): extract to HandlingFutureBuilder
+    return FutureBuilder<bool?>(
+        // TODO(betka): support also non-customizable vibrations (Vibration.hasVibration())
+        future: Vibration.hasCustomVibrationsSupport(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('While finding Vibrator an error occurred.');
+          }
+
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+
+          final hasVibrator = snapshot.data ?? false;
+
+          if (!hasVibrator) {
+            return Text('Your device does not support customized vibrations.');
+          }
+
+          return _buildVibrationContent();
+        });
   }
 
   Widget _buildVibrationContent() {
