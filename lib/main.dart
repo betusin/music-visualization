4b491994fdf931cea67f23f0e8229a4f9f592b0a
Alžbeta Hajná <ahajna@typesoft.io>
2025-02-24 13:57:20 +0100

    Remove unnecessary MyApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,5 @@ import 'package:vibration_poc/ioc/ioc_container.dart';
 
 void main() {
   IocContainer.setup();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return AppRoot();
-  }
+  runApp(const AppRoot());
 }
