a84bbad40a9de800485e9dc01c2c58bfddca0b43
Alžbeta Hajná <ahajna@typesoft.io>
2025-03-15 17:52:51 +0100

    Play/Stop as IconButtons

--- lib/recorder/widget/simple_recorder.dart
+++ lib/recorder/widget/simple_recorder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_kit/stream/widget/handling_stream_builder.dart';
 import 'package:vibration_poc/ioc/ioc_container.dart';
 import 'package:vibration_poc/recorder/service/recorder_controller.dart';
 import 'package:vibration_poc/vibration/widget/vibration_switcher_and_adjuster.dart';
@@ -18,14 +19,23 @@ class SimpleRecorder extends StatelessWidget {
     );
   }
 
-  Row _buildStartStopRecording(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(onPressed: () => _startRecordingToStream(context), child: Text('start recording')),
-        SizedBox(width: 8),
-        ElevatedButton(onPressed: () => _recorderController.stopRecording(), child: Text('stop recording')),
-      ],
+  Widget _buildStartStopRecording(BuildContext context) {
+    return HandlingStreamBuilder(
+      stream: _recorderController.stream,
+      builder: (context, data) {
+        if (data == null) {
+          return ElevatedButton.icon(
+            onPressed: () => _startRecordingToStream(context),
+            icon: Icon(Icons.play_arrow),
+            label: Text('Play'),
+          );
+        }
+        return ElevatedButton.icon(
+          onPressed: () => _recorderController.stopRecording(),
+          icon: Icon(Icons.pause),
+          label: Text('Pause'),
+        );
+      },
     );
   }
 
