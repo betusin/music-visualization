ad3c6a2ae58dc48bbbc397ea55cff53fe166d676
Alžbeta Hajná <ahajna@typesoft.io>
2025-03-24 17:23:44 +0100

    Implement displaying webpage via webview

--- lib/recorder/widget/animation_with_vibration_page.dart
+++ lib/recorder/widget/animation_with_vibration_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vibration_poc/animation/widget/music_animation.dart';
-import 'package:vibration_poc/recorder/widget/simple_recorder.dart';
+import 'package:vibration_poc/web_view/widget/web_page_display.dart';
 
 class AnimationWithVibration extends StatelessWidget {
   const AnimationWithVibration({super.key});
@@ -9,8 +8,10 @@ class AnimationWithVibration extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(child: SimpleRecorder()),
-        Expanded(child: MusicAnimation()),
+        // Expanded(child: SimpleRecorder()),
+        // Expanded(child: MusicAnimation()),
+        // Expanded(child: WebPageDisplay(url: 'https://flutter.dev')),
+        Expanded(child: WebPageDisplay(url: 'http://192.168.1.105:3000/')),
       ],
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import device_info_plus
 import record_darwin
 import shared_preferences_foundation
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
