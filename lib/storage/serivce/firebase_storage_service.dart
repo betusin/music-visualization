11aa2605533407e5a7bbc6f10374ef4dec6f510b
Alžbeta Hajná <ahajna@typesoft.io>
2025-04-14 12:49:09 +0200

    uploadFile only if does not exist

--- lib/storage/serivce/firebase_storage_service.dart
+++ lib/storage/serivce/firebase_storage_service.dart
@@ -8,14 +8,31 @@ const _audioFolder = 'audio';
 class FirebaseStorageService {
   final storage = FirebaseStorage.instanceFor(bucket: "gs://music-vis");
 
-  void uploadFile(FilePickerResult result) {
+  Future<void> uploadFile(FilePickerResult result) async {
     assert(result.count == 1);
 
     final platformFile = result.files.first;
     final fileName = platformFile.name;
 
     final fileRef = storage.ref().child('$_audioFolder/$fileName');
-    // TODO(betka): try-catch
-    fileRef.putFile(File(platformFile.path!));
+    final doesFileExist = await fileExists(fileRef);
+
+    if (doesFileExist) {
+      // TODO(betka): try-catch
+      fileRef.putFile(File(platformFile.path!));
+    }
+  }
+
+  Future<bool> fileExists(Reference storageRef) async {
+    try {
+      await storageRef.getMetadata();
+      return true;
+    } on FirebaseException catch (e) {
+      if (e.code == 'object-not-found') {
+        return false;
+      } else {
+        rethrow;
+      }
+    }
   }
 }
