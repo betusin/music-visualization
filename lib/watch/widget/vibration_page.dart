b074eff0b1f4e832fcdc9952433242c0f0742124
Alžbeta Hajná <ahajna@typesoft.io>
2025-04-26 16:41:50 +0200

    get deviceId from pairLink on watch (multiple devices? - now only gets the first)

--- lib/watch/widget/vibration_page.dart
+++ lib/watch/widget/vibration_page.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:ui_kit/stream/widget/handling_stream_builder.dart';
+import 'package:vibration_poc/common/ui_constants.dart';
+import 'package:vibration_poc/ioc/ioc_container.dart';
+import 'package:vibration_poc/pair/service/pairing_service.dart';
 import 'package:vibration_poc/vibration/widget/vibration_observer.dart';
 import 'package:vibration_poc/watch/widget/page_wrapper.dart';
+import 'package:vibration_poc/watch/widget/pair_with_phone_page.dart';
 
 class VibrationPage extends StatelessWidget {
-  const VibrationPage({super.key});
+  final _pairingService = get<PairingService>();
+
+  VibrationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO(betka): get that from pairLinks
-    final deviceId = "KZGzluk34KRuPm1kEle85UsvAnf2";
-
     return PageWrapper(
       child: Center(
-        child: VibrationObserver(deviceId: deviceId),
+        child: HandlingStreamBuilder(
+          stream: _pairingService.getPairLinksPerCurrentWatch,
+          builder: (context, pairLinks) {
+            if (pairLinks.isEmpty) {
+              return _buildNoPair(context);
+            }
+            final deviceId = pairLinks.first.deviceId;
+            return VibrationObserver(deviceId: deviceId);
+          },
+        ),
       ),
     );
   }
+
+  Widget _buildNoPair(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: smallGapSize,
+      children: [
+        Text('No paired device'),
+        ElevatedButton(onPressed: () => _navigateToPairPage(context), child: Text('Go to Pair page')),
+      ],
+    );
+  }
+
+  Future<dynamic> _navigateToPairPage(BuildContext context) =>
+      Navigator.push(context, MaterialPageRoute(builder: (context) => PairWithPhonePage()));
 }
