1f115e4520c1c09184dd109d29220fda8e3cf38c
Alžbeta Hajná <ahajna@typesoft.io>
2025-05-01 09:58:04 +0200

    property floatingActionButton in PageWrapper

--- lib/watch/widget/page_wrapper.dart
+++ lib/watch/widget/page_wrapper.dart
@@ -4,13 +4,15 @@ import 'package:vibration_poc/common/ui_constants.dart';
 class PageWrapper extends StatelessWidget {
   final Widget child;
   final String? title;
+  final FloatingActionButton? floatingActionButton;
 
-  const PageWrapper({super.key, required this.child, this.title});
+  const PageWrapper({super.key, required this.child, this.title, this.floatingActionButton});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: title == null ? null : AppBar(title: Text(title!)),
+      floatingActionButton: floatingActionButton,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(smallGapSize),
