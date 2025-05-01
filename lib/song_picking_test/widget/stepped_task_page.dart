c9d3d121f7d0708f4d5d9f11d3f277200253697a
Alžbeta Hajná <ahajna@typesoft.io>
2025-05-01 09:59:32 +0200

    Done stepped_task_page with mocked RCH songs

--- lib/song_picking_test/widget/stepped_task_page.dart
+++ lib/song_picking_test/widget/stepped_task_page.dart
@@ -1,9 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:master_kit/sdk_extension/iterable/iterable_extension.dart';
+import 'package:vibration_poc/common/ui_constants.dart';
+import 'package:vibration_poc/common/widget/main_page.dart';
 import 'package:vibration_poc/recorder/widget/preset_visualization.dart';
 import 'package:vibration_poc/watch/widget/page_wrapper.dart';
 
 // TODO(betka): get chosen songs
 const _songIdentifiers = ['12.mp3', '124.mp3'];
+const _songNames = ['New Balance', 'The Dude'];
+final _count = _songIdentifiers.length;
+
+final _shuffledOptions = List.from(_songNames)..shuffle(Random());
 
 class SteppedTaskPage extends StatefulWidget {
   const SteppedTaskPage({super.key});
@@ -15,17 +24,87 @@ class SteppedTaskPage extends StatefulWidget {
 class _SteppedTaskPageState extends State<SteppedTaskPage> {
   // TODO(betka): move these to controller?
   int _currentStep = 0;
+  int _correctAnswers = 0;
   bool _isGuessing = false;
+  String? _currentlySelectedOption;
+
+  bool get _finishedTest => _currentStep >= _count;
 
   @override
   Widget build(BuildContext context) {
     return PageWrapper(
-      title: 'Song Guessing - Step ${_currentStep + 1}',
-      child: PresetVisualization(
-        initialFileId: _songIdentifiers[_currentStep],
-        showFilePicker: false,
-        showVibration: false,
+      title: _finishedTest ? null : 'Song ${_isGuessing ? 'Guessing' : 'Playing'} - Step ${_currentStep + 1}',
+      floatingActionButton: _finishedTest
+          ? null
+          : FloatingActionButton(
+              onPressed: () => _isGuessing && _currentlySelectedOption == null ? null : _nextStep(),
+              child: Icon(Icons.navigate_next),
+            ),
+      child: _buildChild(),
+    );
+  }
+
+  Widget _buildChild() {
+    if (_finishedTest) {
+      return _buildFinishedTest();
+    }
+
+    if (_isGuessing) {
+      return _buildRadioOptions();
+    }
+
+    return PresetVisualization(
+      initialFileId: _songIdentifiers[_currentStep],
+      showFilePicker: false,
+      showVibration: false,
+    );
+  }
+
+  Widget _buildFinishedTest() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: smallGapSize,
+        children: [
+          Text('Congrats! You Finished the test!'),
+          Text('Your score was: $_correctAnswers/$_count'),
+          ElevatedButton(
+              onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MainPage())),
+              child: Text('Try full app')),
+        ],
       ),
     );
+  }
+
+  Widget _buildRadioOptions() {
+    return Column(
+      children: _shuffledOptions.mapToList(
+        (option) => RadioListTile<String>(
+          title: Text(option),
+          value: option,
+          groupValue: _currentlySelectedOption,
+          onChanged: (value) => setState(() => _currentlySelectedOption = value),
+        ),
+      ),
+    );
+  }
+
+  void _nextStep() {
+    if (!_isGuessing) {
+      setState(() => _isGuessing = true);
+      return;
+    }
+
+    final correctAnswer = _songNames[_currentStep];
+    final isCorrect = _currentlySelectedOption == correctAnswer;
+
+    setState(() {
+      _currentStep++;
+      _isGuessing = false;
+      _currentlySelectedOption = null;
+      if (isCorrect) {
+        _correctAnswers++;
+      }
+    });
   }
 }
